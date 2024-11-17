6934a405753ac013f6cd001c5394b77036ae27c4
Diego <pinto.p.diego@gmail.com>
2024-11-17 16:39:32 -0300

    Se crea componentes TodosError y TodosLoading

---
+++ src/TodosLoading/index.js
@@ -0,0 +1,11 @@
+import React from 'react'
+import './TodosLoading.css'
+function TodosLoading() {
+
+  return (<div className='loadingDiv'>
+<p>cargando... </p> 
+<span className="loader"></span> 
+</div>)
+}
+
+export {TodosLoading}
